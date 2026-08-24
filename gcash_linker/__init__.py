9c35be449bb918026a57524ab413e199a19b4813
xiyouxiu1 <100484663+xiyouxiu1@users.noreply.github.com>
2026-08-25 01:00:31 +0800

    更新 Web 控制台 / Update web console

--- gcash_linker/__init__.py
+++ gcash_linker/__init__.py
@@ -1,10 +1,18 @@
 """GCash link extractor building blocks."""
 
+from .identity import (
+    TokenIdentityError,
+    decode_access_token_payload,
+    extract_email_from_access_token,
+)
 from .inputs import BatchRequest, InputValidationError, ProxyPool, parse_batch_request
 
 __all__ = [
     "BatchRequest",
     "InputValidationError",
     "ProxyPool",
+    "TokenIdentityError",
+    "decode_access_token_payload",
+    "extract_email_from_access_token",
     "parse_batch_request",
 ]
